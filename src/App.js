18ddb4de92b92e8b811ebb439d651a0032a94b42
sergeykasyan <s.kasyan@list.ru>
2020-06-29 17:51:26 +0300

    Add first version

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component, Fragment } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>First version</div>
+    );
+  }
+}
+
+export default App;
